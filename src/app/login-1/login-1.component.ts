a5372f2e5ec65391aca970823667aeb11afa8d53
Jonas Martinez <martinezjonas817@gmail.com>
2025-02-09 21:20:07 -0500

    Proyecto inicial - Login

---
+++ src/app/login-1/login-1.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup,  Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+
+
+@Component({
+  selector: 'app-login-1',
+  standalone: true,
+  imports: [ReactiveFormsModule, NzFormModule, NzInputModule, NzButtonModule
+    , NzCardModule, NzCheckboxModule
+  ],
+  templateUrl: './login-1.component.html',
+})
+export class Login1Component {
+
+  loginForm!: UntypedFormGroup;
+
+    submitForm(): void {
+        for (const i in this.loginForm.controls) {
+            this.loginForm.controls[ i ].markAsDirty();
+            this.loginForm.controls[ i ].updateValueAndValidity();
+        }
+    }
+
+    constructor(private fb: UntypedFormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.loginForm = this.fb.group({
+            userName: [ null, [ Validators.required ] ],
+            password: [ null, [ Validators.required ] ]
+        });
+    }
+
+}
