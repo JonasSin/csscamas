fce124df53b291d7e6289722e133193efa739ec7
JonasSin <martinezjonas817@gmail.com>
2025-02-28 15:02:34 -0500

    feat(frontend) desarrollo del dashboard de gestión y sus componentes

--- src/app/gestion/gestion.component.ts
+++ src/app/gestion/gestion.component.ts
@@ -3,11 +3,14 @@ import { Component } from '@angular/core';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 
+import { PisosComponent } from '../pisos/pisos.component';
 
 @Component({
   selector: 'app-gestion',
   standalone: true,
-  imports: [NzCardModule, NzGridModule],
+  imports: [NzCardModule, NzGridModule, 
+    PisosComponent
+  ],
   templateUrl: './gestion.component.html',
   styleUrl: './gestion.component.css'
 })
